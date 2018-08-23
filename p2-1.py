eadb3fad80fbd626a4cd8885175fce334af76efe
edio.gu <1248606295@qq.com>
2018-08-24 01:36:07 +0800

    添加： 	py2-1.py程序，以及github的使用

---
+++ p2-1.py
@@ -0,0 +1,9 @@
+"first game"
+temp = input("不妨猜一下小甲鱼现在心里想的数字是什么呢：")
+guess = int(temp)
+if guess == 8:
+    print("你是小甲鱼心中的蛔虫嘛？？")
+    print("哼。猜中了也没有奖励")
+else:
+    print("猜错了哦，小甲鱼现在心里想的是8")
+print("游戏结束，不玩了")
